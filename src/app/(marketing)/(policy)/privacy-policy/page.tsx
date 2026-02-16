135dbf3d875381ead8a327b264e93e290ee643cd
Ed3hub <ed3hub@gmail.com>
2026-02-16 21:01:41 +0100

    I have deleted everything

--- src/app/(marketing)/(policy)/privacy-policy/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function PrivacyPolicy() {
-    return (
-        <div className="flex flex-col justify-center items-center">
-            <h1>Privacy Policy</h1>
-        </div>
-    )
-}
--- src/components/ui/container.tsx
+++
@@ -1,9 +0,0 @@
-import { cn } from "@/lib/utils";
-
-export const Container = ({ children, className }: { children: React.ReactNode, className?: string }) => {
-    return (
-        <div className={cn("max-w-7xl mx-auto flex flex-col pt-28 space-y-10", className)}>
-                {children}
-        </div>
-    )
-}
